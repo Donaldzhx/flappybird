99a964b0db00ef3328525ec0a982253052bc3c70
Donaldzhx <zx88485575@outlook.com>
2025-05-07 15:33:36 +0800

    build.bat added

---
+++ build.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+set ANDROID_SDK_ROOT=C:\Users\zx884\AppData\Local\Android\Sdk
+set ANDROID_NDK_ROOT=C:\Users\zx884\AppData\Local\Android\Sdk\ndk\29.0.13113456
+set PATH=%ANDROID_SDK_ROOT%\tools;%ANDROID_SDK_ROOT%\platform-tools;%PATH%
+set ADB=C:\Users\zx884\AppData\Local\Android\platform-tools\adb.exe
+
+set APKNAME=FlappyBird
+set ANDROIDVERSION=30
+set ANDROIDTARGET=30
+
+set KEYSTORE_PASSWORD=12345678
+echo Cleaning previous builds ...
+rmdir /s /q app\builds
+
+echo Creating build directories ...
+mkdir app\build\intermediates\ndk
+mkdir app\build\outputs\apk
+
+echo Building native code ...
+cd app\src\main
+call %ANDROID_NDK_ROOT%\ndk-build
+if %errorlevel% neq 0 (
+    echo Error building native code!
+    echo Error code: %errorlevel%
+    exit /b %errorlevel%
+)
+cd ..\..\..
+
+echo Creating empty APK...
+%ANDROID_SDK_ROOT%\build-tools\35.0.1\aapt package -f -M app\src\main\AndroidManifest.xml -S app\src\main\res -A app\src\main\assets -I %ANDROID_SDK_ROOT%\platforms\android-%ANDROIDTARGET%\android.jar -F app\build\outputs\apk\unaligned.apk
+if %errorlevel% neq 0 (
+    echo Error creating empty APK!
+    echo Error code: %errorlevel%
+    exit /b %errorlevel%
+)
+
+mkdir lib
+
+:: Copy files from libs to a temporary folder
+xcopy "app\src\main\libs\*" "lib\" /E /I /Y
+
+:: Add the contents of the temporary folder to the archive in the lib folder
+start /min /wait WinRAR A -r app\build\outputs\apk\unaligned.apk "lib\*"
+
+echo Aligning APK ...
